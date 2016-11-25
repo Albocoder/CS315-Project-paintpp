fa1775ac377cfc2991bd33926be8603a7acacb14
gulsumgudukbay <gulsum.gudukbay@ug.bilkent.edu.tr>
2016-11-25 15:12:08 +0200

    a

---
+++ Paintpp/src/Paintpp.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : Paintpp.c
+ Author      : Gulsum Gudukbay & Melis Kizildemir & Erin Avllazagaj
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
